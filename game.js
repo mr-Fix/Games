5f966f5739e30f6a8b843763212277c3fee430e6
mrfix <potlaker@gmail.com>
2023-02-09 00:07:03 +0400

    + метод загрузки изображенй

--- game.js
+++ game.js
@@ -1,10 +1,52 @@
 let game = {
-    start: function() {
+
+    /** Инициализация */
+    init() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
+    },
+
+    /** Загрузщик игровых объектов */
+    preload() {
+        let loadInfoList = [
+            { src: "img/background.png", coords: [0,0] },
+            { src: "img/ball.png", coords: [0,0] },
+            
+        ];
+
+        let loadAll = loadInfoList.map(loadInfo => new Promise((resolve, reject) => {
+            let image = new Image();
+            image.src = loadInfo.src;
+
+            image.onload = () => {
+                resolve({ img: image, coords: loadInfo.coords, });
+            };
+            image.onerror = (err) => {
+                reject(err);
+            };
+        }));
+
+        Promise.all(loadAll)
+            .then(results => {
+                for(let imgObj of results) {
+                    this.ctx.drawImage(imgObj.img, ...imgObj.coords); 
+                }
+            })
+            .catch(err => console.log(err));
+    },
+
+    start: function() {
+        
+
         let background = new Image();
         background.src = "img/background.png";
         background.onload = () => {
             this.ctx.drawImage(background, 0, 0);
+        };
+
+        let ball = new Image();
+        ball.src = "img/ball.png";
+        ball.onload = () => {
+            this.ctx.drawImage(ball, 0, 0);
         };
     }
 };
