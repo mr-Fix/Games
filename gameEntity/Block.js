3d6912376f1c55ec50156d736c319292c707e571
mrfix <potlaker@gmail.com>
2023-02-20 23:28:35 +0400

    + создание координат для блоков

--- gameEntity/Block.js
+++ gameEntity/Block.js
@@ -6,6 +6,23 @@ export default class Block {
 
         // путь до изображения
         this.imagePath = './gameEntity/img/block.png';
+
+        // ширина
+        this.width = 64
+
+        // высота
+        this.height = 24;
+
+        // Расположение блоков
+        this.positonBlocks = {
+            1: { row: 4, col: 8, },
+        };
+
+        // массив с координатами блоков
+        this.coordsBlock = [];
+
+        // заполняем координаты
+        this.createCoordsBlock();
     }
 
     /** Загрузчик необходимых данных */
@@ -13,5 +30,22 @@ export default class Block {
         this.image = await this.utility.loadImage(this.imagePath);
     }
 
-    
+    /** Создает координаты для блоков */
+    createCoordsBlock() {
+
+        this.coordsBlock = [];
+        
+        for(let i = 0; i < this.positonBlocks['1'].row; ++i) {
+
+            for(let j = 0; j < this.positonBlocks['1'].col; ++j) {
+
+                this.coordsBlock.push([
+                    this.gameEntities.images.block.width * j + 65,
+                    this.gameEntities.images.block.height * i + 35,
+                    true,
+                ]);
+            }
+        }
+
+    }
 }
