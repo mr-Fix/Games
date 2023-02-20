a966024868559d3187b87060818458a9c3f5af33
mrfix <potlaker@gmail.com>
2023-02-20 23:07:16 +0400

    ренейм

---
+++ Utillty.js
@@ -0,0 +1,33 @@
+
+export default class Utility {
+    /**
+     * Генерирует рандомное число в диапазоне от min до max
+     * @param {number} min - минимально допустимое число
+     * @param {number} max - максимально допустимое число
+    */
+    random(min, max) {
+        return Math.floor( Math.random() * (max - min + 1) + min );
+    }
+
+    /**
+     * Загружает изображение по переданному path
+     * @param {string} path - путь до изображения
+     * @returns {object} - объект изображения или ошибки
+     */
+    loadImages(path) {
+
+        return new Promise((resolve, reject) => {
+            let image = new Image();
+            image.src = path;
+
+            image.addEventListener("load", () => {
+                resolve(image);
+            });
+
+            image.addEventListener("error", () => {
+                reject(err);
+            });
+        });
+        
+    }
+}
