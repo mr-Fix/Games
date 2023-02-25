3c1b339d2b81d07b186e4f4e4037f653c9dfcedb
mrfix <potlaker@gmail.com>
2023-02-25 22:26:21 +0400

    + класс звука Bump

--- soundEntity/Bump.js
+++ soundEntity/Bump.js
@@ -1,5 +1,28 @@
 
 /** Класс звук удара */
-export default class Bump {
+class Bump {
+    constructor(utility, parent) {
+        // полезные методы
+        this.utility = utility;
 
+        // родитель (игра)
+        this.parent = parent;
+
+        // загруженный звук
+        this.sound = null;
+
+        // путь до звука
+        this.soundPath = './soundEntity/sounds/bump.mp3';
+    }
+
+    /** Загрузчик необходимых данных */
+    async loadData() {
+        try{
+
+            this.sound = await this.utility.loadSound(this.soundPath);
+        } catch (err) {
+
+            console.log('Ошибка в Bump > loadData > ', err);
+        }
+    }
 }
