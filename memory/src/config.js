073cab9f8bd3f21188b7af2bb14e3fbb29b9f221
mrfix <potlaker@gmail.com>
2023-05-21 16:27:21 +0400

    + игра

---
+++ memory/src/config.js
@@ -0,0 +1,13 @@
+import { CANVAS, } from "phaser"
+
+export default {
+   type: CANVAS,
+   width: 1280,
+   height: 720,
+   rows: 2,
+   cols: 5,
+   cardWidth: 180,
+   cardHeight: 260,
+   idCards: [1, 2, 3, 4, 5,],
+   timeout: 20,
+}
